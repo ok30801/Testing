ab8368cb5e2bac43fc99aab17c777941370f34f1
Oleg <53225808+ok30801@users.noreply.github.com>
2022-04-11 12:24:50 +0500

    Корректировка данных инструкции

--- src/components/Instruction/index.js
+++ src/components/Instruction/index.js
@@ -3,21 +3,21 @@ import {getData} from '../../utils/api'
 import '../../assets/css/style.scss'
 
 const Instruction = () => {
+  const [title, setTitle] = useState([])
   const [text, setText] = useState([])
 
   useEffect(() => {
-    async function loadData() {
-      try {
-        await getData().then(data => setText(data[0].text))
-      } catch (e) {
-          alert('Ошибка подключения')
-        }
-      }
-    loadData();
+    getData().then(data => {
+      setTitle(data[0].title)
+      setText(data[0].text)
+    })
   }, [])
 
   return (
-    <div className="answer__instruction" dangerouslySetInnerHTML={{__html: text}}/>
+    <>
+      <div className="answer__instruction-title" dangerouslySetInnerHTML={{__html: title}}/>
+      <div className="answer__instruction-text" dangerouslySetInnerHTML={{__html: text}}/>
+    </>
   )
 }
 export default Instruction
